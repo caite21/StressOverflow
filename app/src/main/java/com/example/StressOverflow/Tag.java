a576709c2ba22199ebeed864616c3ff48337d92e
Crystal Zhang <crystal7@ualberta.ca>
2023-10-31 23:12:52 -0600

    - Changed project name from myApplication to Stress Overflow - Added Firebase - Added Tag CLass

---
+++ app/src/main/java/com/example/StressOverflow/Tag.java
@@ -0,0 +1,17 @@
+package com.example.StressOverflow;
+
+public class Tag {
+    private String tagName;
+
+    public void Tag(String tagName){
+        this.tagName = tagName;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+}
