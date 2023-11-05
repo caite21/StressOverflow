34082e8861563f957ddfd6199f260f4e50b27a75
Crystal Zhang <crystal7@ualberta.ca>
2023-11-05 15:47:09 -0700

    strings.xml merge

---
+++ app/src/main/java/com/example/StressOverflow/TagList.java
@@ -0,0 +1,14 @@
+package com.example.StressOverflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TagList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag_list);
+    }
+}
